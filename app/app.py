049195b45fe34ab7231ba62b3f3c14a8bf58ce9b
javier <johnnybelesaca@gmail.com>
2021-05-01 01:55:02 -0400

    Task: added imports

--- app/app.py
+++ app/app.py
@@ -1,4 +1,24 @@
-from flask import Flask
+# Python standard libraries
+import json
+import os
+import sqlite3
+
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from db import init_db_command
+from user import User
 
 app = Flask(__name__)
 
