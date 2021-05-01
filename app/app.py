a26e09bb9e40c5a7111d7f74e141665bb4157dfa
javier <johnnybelesaca@gmail.com>
2021-05-01 01:55:21 -0400

    Task: added configuration

--- app/app.py
+++ app/app.py
@@ -20,6 +20,13 @@ import requests
 from db import init_db_command
 from user import User
 
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
 app = Flask(__name__)
 
 
