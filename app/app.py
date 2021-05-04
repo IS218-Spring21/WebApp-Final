df240b1112b0df36f88a9325bf341bed33bebb88
javier <johnnybelesaca@gmail.com>
2021-05-03 22:40:53 -0400

    Task: updated it

--- app/app.py
+++ app/app.py
@@ -1,27 +1,37 @@
 # Python standard libraries
-import json
 import os
 import sqlite3
 
-# Third-party libraries
-from flask import Flask, redirect, request, url_for
+# Third party libraries
+from flask import Flask
 from flask_login import (
     LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
 )
-from oauthlib.oauth2 import WebApplicationClient
-import requests
 
 # Internal imports
-from login.db import init_db_command
-from login.user import User
+from db import init_db_command
+from user import User
+'''
+# Configuration
+GOOGLE_CLIENT_ID = '328754940117-blnf0979a5plol9qphredrdntpgmrsp9.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'HpeG2-6H1vj3NSTsnvYv0jdj'
+GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+'''
 
 # Flask app setup
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return "You must be logged in to access this content.", 403
+
 
 # Naive database setup
 try:
@@ -30,22 +40,9 @@ except sqlite3.OperationalError:
     # Assume it's already been created
     pass
 
+# OAuth2 client setup
+# client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
-# User session management setup
-# https://flask-login.readthedocs.io/en/latest
-login_manager = LoginManager()
-login_manager.init_app(app)
-'''
-# Configuration
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
-
-# OAuth 2 client setup
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
-'''
 
 # Flask-Login helper to retrieve a user from our db
 @login_manager.user_loader
@@ -68,17 +65,13 @@ def index():
         return '<a class="button" href="/login">Google Login</a>'
 
 
-def get_google_provider_cfg():
-    return requests.get(GOOGLE_DISCOVERY_URL).json()
-
-
 @app.route("/login")
 def login():
     # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-    # Use library to construct the request for Google login and provide
+    # Use library to construct the request for login and provide
     # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
@@ -93,14 +86,17 @@ def callback():
     # Get authorization code Google sent back to you
     code = request.args.get("code")
 
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
 
+    # Prepare and send request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
         redirect_url=request.base_url,
-        code=code
+        code=code,
     )
     token_response = requests.post(
         token_url,
@@ -112,13 +108,16 @@ def callback():
     # Parse the tokens!
     client.parse_request_body_response(json.dumps(token_response.json()))
 
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
     userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
-    # You want to make sure their email is verified.
-    # The user authenticated with Google, authorized your
-    # app, and now you've verified their email through Google!
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
     if userinfo_response.json().get("email_verified"):
         unique_id = userinfo_response.json()["sub"]
         users_email = userinfo_response.json()["email"]
@@ -127,13 +126,13 @@ def callback():
     else:
         return "User email not available or not verified by Google.", 400
 
-    # Create a user in your db with the information provided
+    # Create a user in our db with the information provided
     # by Google
     user = User(
         id_=unique_id, name=users_name, email=users_email, profile_pic=picture
     )
 
-    # Doesn't exist? Add it to the database.
+    # Doesn't exist? Add to database
     if not User.get(unique_id):
         User.create(unique_id, users_name, users_email, picture)
 
@@ -152,26 +151,20 @@ def logout():
 '''
 
 with app.app_context():
-    from main import main_routes
+    from app.main import main_routes
 
     app.register_blueprint(main_routes.main_page)
 
 
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=443, ssl_context="adhoc")
+'''
 
-@app.route("/")
-def index():
-    if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.profile_pic
-            )
-        )
-    else:
-        return '<a class="button" href="/login">Google Login</a>'
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    # app.run(ssl_context=('cert.pem', 'key.pem'))
+    app.run(ssl_context="adhoc")
+    # app.run(host="0.0.0.0", debug=True)
+    #serve(app, host='0.0.0.0', port=5000, url_scheme='https')
+'''
