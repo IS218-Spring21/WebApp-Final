c1849817d276a0d502dae8176ae7596ce6141ebf
Ido Tanne <idovader@gmail.com>
2021-05-04 14:07:30 -0400

    Feature: backend socketIO

--- chatroom/app.py
+++ chatroom/app.py
@@ -10,6 +10,7 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['SESSION_TYPE'] = 'filesystem'  # TODO: Change this to redis
 
 Session(app)
+socketIO = SocketIO(app, manage_session=False)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,5 +33,27 @@ def chatroom():
         return redirect(url_for('index'))
 
 
+@socketIO.on('join', namespace='/chat')
+def join(message):
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': "%s has entered the room.".format(session.get('username'))}, room=room)
+
+
+@socketIO.on('text', namespace='/chat')
+def text(message):
+    room = session.get('room')
+    emit('message', {'msg': "%s : %s".format(session.get('username'), message['msg'])}, room=room)
+
+
+@socketIO.on('left', namespace='/chat')
+def left(message):
+    room = session.get('room')
+    username = session.get('username')
+    leave_room(room)
+    session.clear()
+    emit('status', {'msg': "%s has left the room.".format(session.get('username'))}, room=room)
+
+
 if __name__ == '__main__':
-    app.run()
+    socketIO.run(app)
