1e65337ff47b46d5967fef733eaa7620456d7acb
javier <johnnybelesaca@gmail.com>
2021-05-06 15:08:26 -0400

    Task: hardcoded key

--- app/main/main_routes.py
+++ app/main/main_routes.py
@@ -2,7 +2,7 @@
 
 # Python standard libraries
 import json
-import os
+# import os
 
 # Third-party libraries
 from flask import redirect, request, url_for, render_template, Response
@@ -28,9 +28,9 @@ main_page = Blueprint(
 )
 
 # Configuration
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = os.environ.get("GOOGLE_DISCOVERY_URL", None)
+GOOGLE_CLIENT_ID = "328754940117-blnf0979a5plol9qphredrdntpgmrsp9.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "HpeG2-6H1vj3NSTsnvYv0jdj"
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
 # OAuth 2 client setup
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
